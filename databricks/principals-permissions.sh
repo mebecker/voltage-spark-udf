1c7cae226e326c2d12d7d2b29988d1568a866bbd
Mike Becker <mike.becker@gmail.com>
2024-05-24 10:55:49 -0500

    chore: Add group members and update volume permissions

--- databricks/principals-permissions.sh
+++ databricks/principals-permissions.sh
@@ -2,6 +2,7 @@
 users=("mike@mngenvmcap126710.onmicrosoft.com" "admin@mngenvmcap126710.onmicrosoft.com")
 groupName="voltage-users"
 volumeFullName="adb_cus_01.default.voltage"
+sleepInterval=5s
 
 groupId=$(databricks account groups list --output json | jq -r '.[] | select(.displayName == '\"$groupName\"').id')
 
@@ -10,16 +11,21 @@ if [ -z $groupId ]; then
     groupId=$(databricks account groups create --display-name $groupName | jq -r '.id')
 fi
 
+echo "Sleep for $sleepInterval to allow group creation..."
+sleep $sleepInterval
+
 for i in "${users[@]}"
 do
     userId=$(jq -r '.[] | select(.userName == '\"$i\"').id' <<< $(databricks account users list --output json))
     
     #needs to be "compact" json format here or we get "Error: unexpected end of JSON input" ¯\_(ツ)_/¯
-    json=$(jq -c '.Operations[0].value.members[0].value = '\"$userId\"'' json/group-members.json) 
+    userAddJson=$(jq -c '.Operations[0].value.members[0].value = '\"$userId\"'' json/group-members.json) 
     
     echo "Adding user $i ($userId) to group..."
-    databricks account groups patch $groupId --json $json --profile ACCOUNT 
+    databricks account groups patch $groupId --json $userAddJson --profile ACCOUNT 
 done
 
 echo "Updating volume permissions..."
-databricks grants update volume $volumeFullName --json @json/volume-permissions.json
+
+permissionGrantJson=$(jq -c '.changes[0].principal = '\"$groupName\"'' json/volume-permissions.json) 
+databricks grants update volume $volumeFullName --json $permissionGrantJson
